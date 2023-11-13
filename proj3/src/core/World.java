407c64bd4ac9ee38edea4f2bceee7fc730f66bb3
Ashley Pun <ashleypun@berkeley.edu>
2023-11-13 13:12:52 -0800

    got some randomized floors-A

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -12,12 +12,12 @@ public class World {
     private static final Random random = new Random(SEED);
     private int rwidth;
     private int rheight;
+    private int[][] roomMoves;
+    private int size = 3;
 
     private TETile[][] halls;
 
     public World(int width, int height) {
-        rwidth = 5;           /*randomNum(width/5);*/
-        rheight = 4;            /*randomNum(height/5);*/
         board = new TETile[width][height];
 
         for (int x = 0; x < width; x++) {
@@ -25,23 +25,81 @@ public class World {
                 board[x][y] = Tileset.WATER;
             }
         }
-        rectangularRoom(rwidth, rheight);
+        rwidth = randomNum(size);
+        rheight = randomNum(size);
+        worldMoves(width, height);
     }
 
 
+    private void worldMoves(int width, int height) {    // for duplicating rooms across the board
+        for (int x = 0; x < width; x += width/size) {
+            for (int y = 0; y < height; y += height/size) {
+                rectangularRoom(x, y, width, height);
+            }
+        }
+    }
 
     private int randomNum (int bound) {
         return random.nextInt(bound);
     }
 
-    private void rectangularRoom(int width, int height) {
-        TETile[][] rooms = new TETile[width][height];
-        for (int x = 0; x < width; x++) {
-            for (int y = 0; y < height; y++) {
-                rooms[x][y] = Tileset.GRASS;
-            }
+    private void rectangularRoom(int x, int y, int width, int height) {
+        int roomWidth = 5;
+        int roomHeight = 6;
+        roomMoves = new int[][]{
+                {0,randomNum(3)},{2,randomNum(2)},{3,randomNum(4)},{4,randomNum(1)}
+        };
+
+        for (int i = 0; i < roomMoves.length; i++) {
+            int x1 = x;
+            int y1 = y;
+
+            x += roomMoves[i][0] * roomWidth;
+            y += roomMoves[i][1] * roomHeight;
+
+            fill(x, y, roomWidth, roomHeight, width, height);
+
+            x = x1;
+            y = y1;
+
+
+//        int rwid = 5; /*randomNum(width / size);*/
+//        int rhei =6; /* randomNum(height / size);*/
+//
+//        int sizeOfRooms = 5;
+//
+//        int roomWidth = 3; /*rwid + sizeOfRooms;*/
+//        int roomHeight = 4; /*rhei + sizeOfRooms;*/
+//
+//        roomMoves = new int[][]{{roomWidth, roomHeight}    /*, {rwid, rhei}*/
+//        };
+//
+//        for (int i = 0; i < roomMoves.length; i++) {
+//            int x1 = x;
+//            int y1 = y;
+//
+//            x += roomMoves[i][0];
+//            y += roomMoves[i][1];
+//
+//            fill(x, y, width, height);
+//
+//            x = x1;
+//            y = y1;
+
         }
     }
+    private void fill (int x, int y, int roomWidth, int roomHeight, int width, int height){
+        for (int i = x; i<= x+roomWidth+1; i++)
+                for (int j = y; j<= y+roomHeight+1; j++) {
+                    if (i >= 0 && j >= 0 && i < width && j < height) {
+                        board[i][j] = Tileset.FLOWER;
+                    }
+                }
+        }
+
+
+
+
 
     private void hallways(){
 
