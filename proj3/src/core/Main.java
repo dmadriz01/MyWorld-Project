0f7bd785bd54402d15556cec93829e3fd6b9fd29
Ashley Pun <ashleypun@berkeley.edu>
2023-11-12 23:56:27 -0800

    created the world -A

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -1,10 +1,24 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        // build your own world!
+        int WIDTH = 30;
+        int HEIGHT = 30;
 
+        // build your own world!
+        //running world
+
+        World w = new World(WIDTH, HEIGHT);
+
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        ter.renderFrame(w.getTiles());
 
     }
 }
--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,71 @@
 package core;
 
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.util.Random;
+
 public class World {
+    //just the rooms and hallways
+    private static TETile[][] board;
+    private static final long SEED = 2873123;
+    private static final Random random = new Random(SEED);
+    private int rwidth;
+    private int rheight;
+
+    private TETile[][] halls;
+
+    public World(int width, int height) {
+        rwidth = 5;           /*randomNum(width/5);*/
+        rheight = 4;            /*randomNum(height/5);*/
+        board = new TETile[width][height];
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                board[x][y] = Tileset.WATER;
+            }
+        }
+        rectangularRoom(rwidth, rheight);
+    }
+
+
+
+    private int randomNum (int bound) {
+        return random.nextInt(bound);
+    }
+
+    private void rectangularRoom(int width, int height) {
+        TETile[][] rooms = new TETile[width][height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                rooms[x][y] = Tileset.GRASS;
+            }
+        }
+    }
+
+    private void hallways(){
+
+    }
+
+    private void addborders(){
+
+    }
+
+
+    public TETile[][] getTiles() {
+        return board;
+    }
+
+
+
 
     // build your own world!
+
+
+//    helper method just for rooms
+    // helper method just for hallways
+
+    //generate all rooms and hallways connection
+
 
 }
