6c7f41b2a38692d1c5b6223485b2060827d31360
Ashley Pun <ashleypun@berkeley.edu>
2023-11-16 11:57:12 -0800

    world error

--- proj3/README.md
+++ proj3/README.md
@@ -1,10 +1,24 @@
 # Build Your Own World Design Document
 
-**Partner 1:**
+**Partner 1: Ashley Pun
 
-**Partner 2:**
+**Partner 2: David Madriz
 
 ## Classes and Data Structures
+worldMoves(int width, int height) - splits the board into a grid of 5x5 so i can iterate through
+
+buildRectangularRoom(int x, int y, int width, int height) - building random rectangular rooms
+
+randomNum (int bound) - generates random number
+
+addBorders(int width, int height) - add borders on the underlying boolean array containing all values that are rooms and hallways 
+
+(in progress)
+buildhallways - builds hallways from values (bottom left corner of each room) stored in hashmap
+
+(in progress)
+isConnected - boolean value to show if room is connected or not
+
 
 ## Algorithms
 
--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -7,31 +7,15 @@ import java.util.*;
 
 
 public class World {
-    //just the rooms and hallways
     private static TETile[][] board;
     private static final long SEED = 2873123;
     private static final Random random = new Random(SEED);
-    private int width;
-    private int height;
-    private int[][] roomMoves;
-    private int size = 5;
     private int scale = 5; //comparable to holesize
 
     private TETile[][] halls;
     private static boolean[][] bboard;
 
-//    protected class Coordinate{
-//        int x;
-//        int y;
-//        public Coordinate(int x, int y){
-//            this.x = x;
-//            this.y = y;
-//        }
-//    }
-
-
     private HashMap<Integer, Integer> roomMap;
-//    private HashMap<Integer, Integer> roomdimensions;
 
 
 
@@ -40,24 +24,14 @@ public class World {
         board = new TETile[width][height];
         bboard = new boolean[width][height];
         roomMap = new HashMap<>();
-//        roomdimensions = new HashMap<>();
-
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 board[x][y] = Tileset.WATER;
             }
         }
-//        rwidth = randomNum(size);
-//        rheight = randomNum(size);
         worldMoves(width, height, scale);
         addBorders(width, height);
-
-//        for (Integer name: roomdimensions.keySet()) {
-//            String key = name.toString();
-//            String value = roomdimensions.get(name).toString();
-//            System.out.println(key + " " + value);
-//        }
     }
 
 
@@ -79,10 +53,6 @@ public class World {
         int roomWidth = randomNum((width / scale)-3+1)+3;
         int roomHeight = randomNum((height / scale)-3+1)+3;
 
-//        board[x][y] = Tileset.FLOWER;
-
-//        List<Integer> valy = new ArrayList<>();
-
         for (int i = x; i <= x+roomWidth; i++){
             for(int j=y; j <= y+roomHeight; j++){
                 if (i >= 0 && j >= 0 && i < width && j < height) {
@@ -91,8 +61,6 @@ public class World {
                 }
             }
         }
-
-//        roomdimensions.put(roomWidth,roomHeight);
         roomMap.put(x,y);
         board[x][y] = Tileset.SAND;
     }
@@ -104,11 +72,6 @@ public class World {
 //        helpoer method to build hallway from anchor point
 //        make sure anchor point is not in room
         
-
-
-    }
-
-    private void fillroom (int width, int height){
 
 
     }
@@ -137,106 +100,6 @@ public class World {
                 }
             }
         }
-//    }
-
-
-
-
-
-//        roomMoves = new int[][]{
-//                {randomNum(width/size),randomNum(height/size)}    /*,{2,randomNum(2)},{3,randomNum(4)},{4,randomNum(1)*/
-//        };
-//        for (int i=0; i< roomMoves.length;i++){
-//
-//        }
-//
-//        for (int i = 0; i < roomMoves.length; i++) {
-//            int x1 = x;
-//            int y1 = y;
-//
-//            x += roomMoves[i][0] * roomWidth;
-//            y += roomMoves[i][1] * roomHeight;
-//
-//
-//            fill(x, y, roomWidth, roomHeight, width, height);
-//            addborders(x,y, roomWidth, roomHeight);
-//
-//            x = x1;
-//            y = y1;
-
-
-//        int roomWidth = 1;
-//        int roomHeight = 1;
-//        roomMoves = new int[][]{
-//                {randomNum(2),randomNum(3)}    /*,{2,randomNum(2)},{3,randomNum(4)},{4,randomNum(1)*/
-//        };
-//
-//
-//        for (int i = 0; i < roomMoves.length; i++) {
-//            int x1 = x;
-//            int y1 = y;
-//
-//            x += roomMoves[i][0] * roomWidth;
-//            y += roomMoves[i][1] * roomHeight;
-//
-//
-//            fill(x, y, roomWidth, roomHeight, width, height);
-//            addborders(x,y, roomWidth, roomHeight);
-//
-//            x = x1;
-//            y = y1;
-
-
-//        int rwid = 5; /*randomNum(width / size);*/
-//        int rhei =6; /* randomNum(height / size);*/
-//
-//        int sizeOfRooms = 5;
-//
-//        int roomWidth = 3; /*rwid + sizeOfRooms;*/
-//        int roomHeight = 4; /*rhei + sizeOfRooms;*/
-//
-//        roomMoves = new int[][]{{roomWidth, roomHeight}    /*, {rwid, rhei}*/
-//        };
-//
-//        for (int i = 0; i < roomMoves.length; i++) {
-//            int x1 = x;
-//            int y1 = y;
-//
-//            x += roomMoves[i][0];
-//            y += roomMoves[i][1];
-//
-//            fill(x, y, width, height);
-//
-//            x = x1;
-//            y = y1;
-
-//        }
-//    }
-    private void fill (int x, int y, int roomWidth, int roomHeight, int width, int height){
-        for (int i = x; i<= x+roomWidth+1; i++)
-                for (int j = y; j<= y+roomHeight+1; j++) {
-                    if (i >= 0 && j >= 0 && i < width && j < height) {
-                        board[i][j] = Tileset.FLOWER;
-                    }
-                }
-        }
-
-
-
-
-
-    private void hallways(){
-
-    }
-
-    private void addborders(int x, int y, int width, int height){
-        for (int i = x; i<=width; i++){
-//            for (int j=y; j<=height; j++){
-            board[i][y] = Tileset.FLOOR;
-
-        }
-//        board[x][y] = Tileset.FLOOR;
-    }
 
 
     public TETile[][] getTiles() {
